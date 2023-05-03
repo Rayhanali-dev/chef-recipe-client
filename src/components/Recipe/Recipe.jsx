12183d5ece30b4b887672da040c6d2527c4fcc2c
Rayhanali-dev <rayhanalidev@gmail.com>
2023-05-04 02:24:41 +0600

    recipe card added

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 const Recipe = () => {
-    const recipe = useLoaderData(); 
+    const recipe = useLoaderData();
     console.log(recipe);
-    const {picture_url, name, } = recipe
+    const { picture_url, name, description, likes, years_of_experience, recipes } = recipe;
+    console.log(description);
     return (
-        <div className='h-[600px] relative'>
-            <img className='w-full h-full object-cover' src={picture_url} alt="" />
-            <div className='absolute w-full h-full top-0 left-0 backdrop-brightness-50'>
-
+        <div>
+            <div className='h-[600px] relative'>
+                <img className='w-full h-full object-cover' src={picture_url} alt="" />
+                <div className='absolute flex flex-col justify-center items-center w-full h-full top-0 left-0 backdrop-brightness-50'>
+                    <div className='text-center max-w-7xl mx-auto'>
+                        <h3 className='text-5xl text-white mb-6'>{name}</h3>
+                        <p className='text-white text-xl px-44'>{description}</p>
+                        <p className='text-white text-xl my-3'>Recipes: {recipes.length}</p>
+                        <p className='text-white text-xl'>Experience: {years_of_experience}</p>
+                        <p className='text-white text-xl mt-3'>Likes: ${likes}</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                {
+                    recipes.map(recipe => <RecipeCard key={recipe.recipe_id} recipe={recipe}></RecipeCard>)
+                }
             </div>
         </div>
     );
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ViewRecipe from '../ViewRecipe/ViewRecipe';
 
-const RecipeCard = ({recipe}) => {
-    const {recipes} = recipe;
-    console.log(recipes);
+const RecipeCard = ({ recipe }) => {
+    console.log(recipe);
     return (
         <div>
-            {
-                recipes.map(viewRecipe => <ViewRecipe key={viewRecipe.id} viewRecipe={viewRecipe}></ViewRecipe>)
-            }
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
