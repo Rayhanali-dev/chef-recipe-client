50136424eb056c47844cab8848be154aa90a1854
Rayhanali-dev <rayhanalidev@gmail.com>
2023-05-04 14:37:45 +0600

    hide firebase config

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../provides/AuthProvider';
 
 
